c6fc160b81d8db0fe82938bf1051502b31cf914e
eokulov <evgenyokulov@yandex.ru>
2021-04-06 21:18:48 +0500

    нет изменений

--- urok6.py
+++ urok6.py
@@ -1,10 +1,14 @@
 from pprint import pprint
+from intertools import product
+
 class Person:
     def __init__(self, name, rost, ves, mesto_uch):
         self.name, self.rost, self.ves, self.mesto_uch = name, rost, ves, mesto_uch
         self.key = (name, mesto_uch)
     def __repr__(self):
         return "Person('%s',%s,%s,'%s')" % (self.name, self.rost, self.ves, self.mesto_uch)
+
+
 evgen = Person("evgen", 194, 80, "pnipu")
 ivan = Person("Ivan", 176, 90, "psu")
 naruto = Person("Naruto", 164, 69, "shtn")
@@ -15,3 +19,12 @@ people = {
 }
 pprint(people)
 pprint(people[evgen.key])
+
+queries = [
+'name evgen',
+'ves 90',
+'выше 180',
+"ниже 170",
+"учится в pnip",
+'зовут Naruto'
+]
