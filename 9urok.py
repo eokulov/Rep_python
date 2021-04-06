e3f97184638daca784568f58f0c2d6edc3c84a6c
eokulov <evgenyokulov@yandex.ru>
2021-04-06 21:42:17 +0500

    Добавлена цикличность

--- 9urok.py
+++ 9urok.py
@@ -1,16 +1,18 @@
 import numpy as np
 from pprint import pprint
 
-def answer(quest):
-    q = set(quest.split())
+
+def answer(q):
     print(q)
     if "?" in q:
         x = np.random.randint(low=100, size = 1)
-        if x > 75:
+        if x > 50:
             pprint ("YES")
         else:
             pprint("NO")
-    return print(x)
 
-question = input("Введите свой вопрос: ") + " ?"
-answer(question)
+
+question = ''
+while question != 'стоп':
+	question = input("Введите свой вопрос: ")
+	answer(question)
